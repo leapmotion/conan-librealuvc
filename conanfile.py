30055d98905bdc660e9a66712e799b4ee4753f0d
Darren Buller <darren.buller@ultraleap.com>
2020-05-09 11:37:01 +0100

    Retry using includes

--- conanfile.py
+++ conanfile.py
@@ -36,17 +36,14 @@ class LibrealuvcConan(ConanFile):
         # properly
         tools.replace_in_file("librealuvc/CMakeLists.txt", "project(librealuvc LANGUAGES CXX C)",
                               '''project(librealuvc LANGUAGES CXX C)
-#set(BUILD_WITH_CONAN ON)                              
-#if(EXISTS ${CMAKE_BINARY_DIR}/conanbuildinfo_multi.cmake)
-#  include(${CMAKE_BINARY_DIR}/conanbuildinfo_multi.cmake)
-#else()
-#  include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+set(BUILD_WITH_CONAN ON)                              
+if(EXISTS ${CMAKE_BINARY_DIR}/conanbuildinfo_multi.cmake)
+  include(${CMAKE_BINARY_DIR}/conanbuildinfo_multi.cmake)
+else()
+  include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 #endif()
-include(${CMAKE_BINARY_DIR}/conan_paths.cmake)
-message(STATUS "CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH}")
-set(OpenCV_DIR "${CONAN_OPENCV_ROOT}/cmake")
-message(STATUS "OpenCV_DIR ${OpenCV_DIR}")
-#conan_basic_setup(TARGETS)''')
+set(CMAKE_VERBOSE_MAKEFILE ON)
+conan_basic_setup(TARGETS)''')
 
     def config_options(self):
         """Remove fPIC option on Windows platform
